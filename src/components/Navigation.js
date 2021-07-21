a410038ff73ef9d864e221a3ca8f5d07830961b3
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-07-21 13:00:25 +0200

    Added styles to Navbar and link routes

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -30,9 +30,27 @@ const Navegation = () => {
                 EGG
             </Link>
             <div className="px-4 cursor-pointer md:hidden">
-            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" /></svg>
-
+							<svg 
+								className="w-6 h-6"
+								fill="none" 
+								stroke="currentColor" 
+								viewBox="0 0 24 24" 
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path 
+									strokeLinecap="round" 
+									strokeLinejoin="round" 
+									strokeWidth={2} 
+									d="M4 6h16M4 12h16M4 18h16"
+								/>
+							</svg>
             </div>
+						<div className="pr-9 md:block hidden">
+							<Link className="p-4" to='/'>Home</Link>
+							<Link className="p-4" to='/menu'>Menu</Link>
+							<Link className="p-4" to='/about'>About</Link>
+							<Link className="p-4" to='/content'>Content</Link>
+						</div>
         </nav>
 
     )
