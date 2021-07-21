080e0c49ec1d3f767d7e04d88386532c5379bdf5
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-07-21 16:43:39 +0200

    Updated Hero buton information and styles

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,8 +9,8 @@ const Hero = () => {
                     CeCe's Blog
                 </h1>
                 <Link className='py-6 px-10 bg-yellow-500 rounded-full text-2xl hover:bg-yellow-300 transition duration-300 ease-in-out flex intems-center animate-bounce'>
-                    My passion in writen form
-                    {/* <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" /></svg> */}
+                    Inch by inch 
+                    <svg className="w-6 h-6 ml-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" /></svg>
                 </Link>
             </div>
         </div>
