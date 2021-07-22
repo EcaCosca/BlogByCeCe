4f5ac4c46c7d0a1743e3662d17ef9eb45885f97b
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-07-22 15:15:42 +0200

    Cleaning code on navigation component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,28 +3,6 @@ import { Link } from 'react-router-dom'
 
 const Navegation = ({toggle}) => {
     return (
-
-        // BOOTSTRAP NAV MODEL 
-        // <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        //     <div className="container-fluid">
-        //         <Link className="navbar-brand" to="/">Camila Colonel</Link>
-                
-        //         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        //             <span className="navbar-toggler-icon"></span>
-        //         </button>
-        //         <div className="collapse navbar-collapse" id="navbarNav">
-        //             <ul className="navbar-nav ml-auto">
-        //                 <li className="nav-item">
-        //                     <Link className="nav-link active" to="/create">Create Note</Link>
-        //                 </li>
-        //                 <li className="nav-item">
-        //                     <Link className="nav-link active" to="/user">Create User</Link>
-        //                 </li>
-        //             </ul>
-        //         </div>
-        //     </div>
-        // </nav>
-
         <nav 
 					className='flex justify-between items-center h-16 bg-white text-red relative shadow-sm font-mono'
 					role='navigation'
@@ -55,7 +33,6 @@ const Navegation = ({toggle}) => {
 							<Link className="p-4" to='/content'>Content</Link>
 						</div>
         </nav>
-
     )
 }
 
