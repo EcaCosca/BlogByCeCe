d974c7943da245b4209f06949cace3648c3853a1
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-07-26 16:50:12 +0200

    added a quote to content section

--- src/components/Content.js
+++ src/components/Content.js
@@ -10,7 +10,7 @@ export const Content = () => {
                 <h1 className="text-9xl uppercase font-black">Content</h1>
             </div>
             <div className="flex-col items-center font-mono">
-            <div className="h-screen flex flex-col justify-center items-center bg-gradient-to-b from-red-300 to-transparent">
+            <div className="h-screen flex flex-col justify-center items-center bg-gradient-to-b from-red-300 via-red-300 to-transparent">
                 <h2 className="text-5xl mb-2 p-10">Como un básico se transforma en una prenda exótica</h2>
                 <h2 className="text-3xl mb-1 p-4">Domingo Domingo</h2>
                 <h4 className="text-2xl mb-1 p-4">De Argentina al Universo, el salto que define a Domingo es Domingo</h4>
@@ -31,8 +31,8 @@ export const Content = () => {
                 </div>
                 <div className="flex flex-col justify-center items-center mb-5 p-16 text-2xl">
                     <p className="mb-2">Teniendo América Latina y toda su exquisita cultura como referencia no dejamos  de pensar en Visión- Diseño – Función para describir a esta marca para hombres, que con cada temporada atemporal, nos hace respirar creatividad exponencial y liderazgo como producto nacional.
-                    Agustina Alvez, diseñadora y emprendedora, es quien hace crecer esta marca hace ocho años. Cuando la conocimos nos transmitió una  vibra fresca y ese gran frenesí  que caracteriza a su marca. De tal palo, tal astilla. Durante la entrevista, Agus, hizo mucho hincapié en que no cualquiera usa Domingo.  Detrás de cada colección hay una búsqueda, un estudio.. una propuesta que va más allá de cualquier tendencia.
-                    Todas las colecciones son extremadamente diferentes pero siempre sosteniendo la filosofía de Domingo: prendas de uso diario pero con un éxtasis de estampas y telas un tanto extravagantes. Es por esto que cada colección  tiene su propio packaging, su propia etiqueta y su propia estrategia de marketing.
+                    <b>Agustina Alvez</b>, diseñadora y emprendedora, es quien hace crecer esta marca hace ocho años. Cuando la conocimos nos transmitió una  vibra fresca y ese gran frenesí  que caracteriza a su marca. De tal palo, tal astilla. Durante la entrevista, Agus, hizo mucho hincapié en que no cualquiera usa Domingo.  Detrás de cada colección hay una búsqueda, un estudio.. una propuesta que va más allá de cualquier tendencia.
+                    Todas las colecciones son extremadamente diferentes pero siempre sosteniendo la filosofía de <b>Domingo</b>: prendas de uso diario pero con un éxtasis de estampas y telas un tanto extravagantes. Es por esto que cada colección  tiene su propio packaging, su propia etiqueta y su propia estrategia de marketing.
                     </p>
                 </div>
             </div>
@@ -48,14 +48,21 @@ export const Content = () => {
                         </div>
                     </div>
                     <div className="flex flex-col justify-center items-center mb-5 p-16 text-2xl">
-                        <p className="mb-10">El cliente de Domingo la elige por lo diferente, el algodón de pima peruano,  la textura de un bordado, el detalle en cada avío y la estampa que tanto caracteriza  a esta marca; Pantalones cuadriculados con forrería floreada,  ponchos con guardas mapuches y pilotos acharolados con capucha. Domingo centra su diseño en morfologías básicas, tales como un jogging, pero con la vuelta Dominguera: Un jogging de terciopelo con un cordón trenzado de colores haciendo analogía a los colores del cerro de siete colores, Jujuy, Argentina.
-                        Pero eso no es todo. En Domingo podemos encontrar diferentes colecciones, tales como la Colección Guayaquil, Mardel, Folklore, Atalaya y nuestra favorita, Río. Todas hacen referencia a un lugar o movimiento cultural latinoamericano.
-                        Para darles un ejemplo, la Colección Río, Verano 2016, esta compuesta por prendas con grabados y determinaciones que hacen referencia a la palabra, RÍO, como Río de Janerio, con sus playas y sus avenidas, Rio Paraná y su vista desde Brasil o Argentina, o simplemente juega con el verbo Reír. Es una colección, como todas, rica por la diversidad y su key to success: llena de imágenes que transportan a un recorrido a través de las prendas.
-                        Con 3.000 productos, mínimo, por colección, Domingo se caracteriza por la fidelidad en el proceso y el desarrollo del producto. Estamos hablando de una marca que respeta cada sector de elaboración fijando su distinción en la confección: El textil se busca y se crea una prenda especifica para poder transmitir la base principal de la marca,  multiplicidad y comodidad. 
-                        “El buen Diseñador es el que trabaja para el otro, nuestro objetivo es mostrar la impronta de uno para llegar a todos”  Agustina Alvez
+                        <p className="mb-10">
+                            El cliente de <b>Domingo</b> la elige por lo diferente, el algodón de pima peruano,  la textura de un bordado, el detalle en cada avío y la estampa que tanto caracteriza  a esta marca; Pantalones cuadriculados con forrería floreada,  ponchos con guardas mapuches y pilotos acharolados con capucha. Domingo centra su diseño en morfologías básicas, tales como un jogging, pero con la vuelta Dominguera: Un jogging de terciopelo con un cordón trenzado de colores haciendo analogía a los colores del cerro de siete colores, Jujuy, Argentina.
+                            <br />
+                            Pero eso no es todo. En Domingo podemos encontrar diferentes colecciones, tales como la Colección Guayaquil, Mardel, Folklore, Atalaya y nuestra favorita, Río. Todas hacen referencia a un lugar o movimiento cultural latinoamericano.
+                            <br />
+                            Para darles un ejemplo, la Colección Río, Verano 2016, esta compuesta por prendas con grabados y determinaciones que hacen referencia a la palabra, RÍO, como Río de Janerio, con sus playas y sus avenidas, Rio Paraná y su vista desde Brasil o Argentina, o simplemente juega con el verbo Reír. Es una colección, como todas, rica por la diversidad y su key to success: llena de imágenes que transportan a un recorrido a través de las prendas.
+                            <br />
+                            Con 3.000 productos, mínimo, por colección, Domingo se caracteriza por la fidelidad en el proceso y el desarrollo del producto. Estamos hablando de una marca que respeta cada sector de elaboración fijando su distinción en la confección: El textil se busca y se crea una prenda especifica para poder transmitir la base principal de la marca,  multiplicidad y comodidad. 
                         </p>
                     </div>
                 </div>
+                <div className="flex flex-col justify-center items-center bg-gradient-to-b from-transparent via-red-100 to-transparent">
+                    <h2 className="text-2xl mb-1 p-4"><em>“El buen Diseñador es el que trabaja para el otro, nuestro objetivo es mostrar la impronta de uno para llegar a todos”</em></h2>
+                    <h2 className="text-5xl mb-2 p-10">Agustina Alvez</h2>
+                </div> 
                 <div className="flex flex-row-reverse justify-around p-4">
                     <div className="min-h-screen py-6 flex flex-col justify-center sm:py-12 flex-none px-8">
                         <div   div className="relative py-3 sm:max-w-xl sm:mx-auto">
