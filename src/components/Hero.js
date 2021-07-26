fc2135fb14e5465d0bf0e3afc85c555ace647f64
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-07-26 13:48:10 +0200

    changed the hero component link color from yellow to red

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,9 +6,9 @@ const Hero = () => {
         <div>
             <div className="bg white h-screen flex flex-col justify-center items-center">
                 <h1 className="lg:text-9xl md:text-7xl sm:text-5xl font-black mb-14">
-                    CeCe's Blog
+                    CeCe
                 </h1>
-                <Link className='py-6 px-10 bg-yellow-500 rounded-full text-2xl hover:bg-yellow-300 transition duration-300 ease-in-out flex intems-center animate-bounce'>
+                <Link className='py-6 px-10 bg-red-400 rounded-full text-2xl hover:bg-red-300 transition duration-300 ease-in-out flex intems-center animate-bounce'>
                     Inch by inch 
                     <svg className="w-6 h-6 ml-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" /></svg>
                 </Link>
