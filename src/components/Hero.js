fc40c556936a8d58be157867811d7bc0fd61b3fe
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-07-27 17:04:37 +0200

    Changing letter spacing in Hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ const Hero = () => {
     return (
         <div>
             <div className="bg white h-screen flex flex-col justify-center items-center">
-                <h1 className="lg:text-9xl md:text-7xl sm:text-5xl font-black mb-14">
+                <h1 className=" tracking-widest lg:text-9xl md:text-7xl sm:text-5xl font-black mb-14">
                     CeCe
                 </h1>
                 <Link className='py-6 px-10 bg-red-400 rounded-full text-2xl hover:bg-red-300 transition duration-300 ease-in-out flex intems-center animate-bounce'>
