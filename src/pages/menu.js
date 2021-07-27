2a8d944e81056c852cd12c2d893bc89e59a2bdd6
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-07-27 17:24:39 +0200

    Installed AnimatedTXT package and tested in menu

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Phrase, Char } from "animatedtxt";
+
 
 const menu = () => {
     return (
         <div className="h-screen flex justify-center items-center bg-green-300">
-            <h1 className=" tracking-widest text-9xl uppercase font-black">Menu</h1>
+            <h1 className=" tracking-widest text-9xl uppercase font-black">
+                <Phrase margin={20} size={200} duration={1}>
+                    <Char char="M" />
+                    <Char char="E" />
+                    <Char char="N" />
+                    <Char char="U" />
+                </Phrase>
+            </h1>
         </div>
     )
 }
