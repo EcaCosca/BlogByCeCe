849a840c2d8a55d34ffb146fcbf7e412cbd32b34
EcaCosca <enriquecoscarelli@gmail.com>
2021-07-27 22:10:56 +0200

    adding documents folder

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,7 @@ const Hero = () => {
         <div>
             <div className="bg white h-screen flex flex-col justify-center items-center">
                 <h1 className="lg:text-9xl md:text-7xl sm:text-5xl font-black mb-14">
-                    CeCe's Blog
+                    CeCe
                 </h1>
                 <Link className='py-6 px-10 bg-yellow-500 rounded-full text-2xl hover:bg-yellow-300 transition duration-300 ease-in-out flex intems-center animate-bounce'>
                     Inch by inch 
