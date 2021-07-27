7f6d58616d975939fb4d4ccbe10db2ebc8d1dcff
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-07-27 15:52:18 +0200

    Stylking HOVER AND TRANSFORM TO IMG in about

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,9 +11,9 @@ const About = () => {
              <div className="flex flex-col-reverse justify-around p-4">
                 <div className="min-h-screen py-6 flex flex-col justify-center sm:py-12 flex-none px-8">
                     <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-                        <div className="absolute inset-0 bg-gradient-to-r from-yellow-300 via-yellow-500 to-red-400 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-                        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
-                            <div className="max-w-md mx-auto">
+                        <div className="absolute inset-0 bg-gradient-to-r from-yellow-300 via-yellow-500 to-red-400 shadow-lg hover:shadow-2xl transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl hover:scale-125 hover:rotate-6 ease-in-out duration-500"></div>
+                        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20 hover:shadow-2xl transform hover:scale-105 opacity-90 hover:opacity-100 ease-in-out duration-500">
+                            <div className="max-w-md mx-auto transform hover:scale-105 hover:shadow-2xl hover:opacity-100 ease-in-out duration-500">
                                 <img src={hero} className="rounded-lg" alt="image1" />
                             </div>
                         </div>  
